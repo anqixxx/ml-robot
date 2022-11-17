59eb82b727927696bbbb81a8d259e2dcf2e4b077
anqixxx <anqix2002@gmail.com>
2022-11-16 20:16:12 -0800

    Added movement and learned to interface w camera

---
+++ src/my_controller/node/move.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+
+rospy.init_node('topic_publisher')
+pub = rospy.Publisher('/R1/cmd_vel', Twist, 
+  queue_size=1)
+rate = rospy.Rate(2)
+move = Twist()
+move.linear.x = 0.5
+move.angular.z = 0.0
+
+while not rospy.is_shutdown():
+   pub.publish(move)
+   rate.sleep()
---
+++ src/my_controller/node/score_interface.py
@@ -0,0 +1,23 @@
+import rospy
+from geometry_msgs.msg import Twist
+from std_msgs.msg import String
+
+# Messages to the publisher must be in the form: 
+#     str('TeamRed,multi21,4,XR58')
+# To start the timer, we must use license plate location 0
+# Similarly the last message your robot must publish must use license plate location -1.
+rospy.init_node('topic_publisher')
+pub = rospy.Publisher("license_plate", String, queue_size=1)
+rate = rospy.Rate(2)
+
+location = "0"
+id = "0000"
+tracked = "team11, team11" + ","+ location + "," + id
+
+while not rospy.is_shutdown():
+   pub.publish(tracked)
+   rate.sleep()
+
+location = "-1"
+tracked = "team11, team11" + ","+ location + "," + id
+pub.publish(tracked)
